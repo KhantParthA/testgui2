6f00ceb576673df474bc6a7d8d6f3d9815e5dcea
KhantParthA <parthkhant1@outlook.com>
2019-07-23 18:50:40 +0530

    RealTime Techno with Subscription

--- src/App.js
+++ src/App.js
@@ -5,39 +5,40 @@ import './css/app.css'
 const Count = props => {
   return (
     <div>
-      <div id="block1">
-        <hr />
-        <button id="like" value={props.choise} onClick={() => props.userChoise(props.choise)}>{props.choise}</button>
-        <button id="subscribe" value="voting" onClick={() => props.userChoise('vote')}>subscribeMe {props.language.vote}</button><hr />
+      <div class="blocks"><h1 class="heading1">Store</h1>
+        <hr/>
+        <button id="subscribe" value="voting" onClick={() => props.read({name: 'subscribe', rowno: 0})}>subscribeMe{props.subscription}</button><hr/>
         <div>
-          <div id="info">{props.description}</div>
-          <button id="ably" value={props.language[0].name} onClick={() => props.read('Ably')}>{props.language[0].name}</button>
-          <button id="pusher" value={props.language[1].name} onClick={() => props.read('Pusher')}>{props.language[1].name}</button>
-          <button id="fanouts" value={props.language[2].name} onClick={() => props.read('Fanouts')}>{props.language[2].name}</button>
-          <button id="hydna" value={props.language[3].name} onClick={() => props.read('Hydna')}>{props.language[3].name}</button>
-          <button id="pubnub" value={props.language[4].name} onClick={() => props.read('Pubnub')}>{props.language[4].name}</button>
-          <button id="realtime.co" value={props.language[5].name} onClick={() => props.read('Realtime.co')}>{props.language[5].name}</button>
-          <button id="reappt" value={props.language[6].name} onClick={() => props.read('Reappt')}>{props.language[6].name}</button>
-          <button id="streamdata.io" value={props.language[7].name} onClick={() => props.read('Streamdata.io')}>{props.language[7].name}</button>
-          <button id="tambur.io" value={props.language[8].name} onClick={() => props.read('Tambur.io')}>{props.language[8].name}</button>
-          <button id="convergence" value={props.language[9].name} onClick={() => props.read('Convergence')}>{props.language[9].name}</button>
-          <button id="Meteor" value={props.language[10].name} onClick={() => props.read('Meteor')}>{props.language[10].name}</button><br/>
-          <hr />
+        <h3 class="heading2">Description</h3>
+          <div id="info">
+          {props.description}
+          </div><hr/>
+          {props.extensions.map( (extension, index) => 
+            <div class="extensions"><div class="extensionname" onClick={() => props.read({name: extension.name, rowno: index})}>{extension.name}</div><hr/>
+            <button id={extension.name} class="addbutton" value={extension.name} onClick={() => props.read(index)}>{extension.added ? "Remove" : "Add"}</button>
+            </div>    
+          )}
+          <hr/>
         </div>
+      </div>
+      <div class="blocks"><h1 class="heading1">Desktop</h1>
+        {props.added.map(extension => 
+          <div>{extension}</div>  
+        )}
       </div>
     </div>    
   )
 }
 const mapState = state => ({
   choise: state.select.choise,
-  vote: state.select.vote,
-  language: state.select.language,
-  description: state.select.description
+  subscription: state.select.subscription,
+  extensions: state.select.extensions,
+  description: state.select.description,
+  added: state.select.added,
 })
 
 const mapDispatch = dispatch => ({
-  userChoise: (data) => dispatch.select.choise(data),
-  read: (subject) => dispatch.select.read(subject) 
+  read: (data) => dispatch.select.read(data) 
 })
 
 export default connect(mapState, mapDispatch)(Count)
--- src/models/select.js
+++ src/models/select.js
@@ -1,33 +1,34 @@
 export default {
     state: {
-      choise: 'Like',
-    language: [
-      {name: 'Ably', vote: 0}, 
-      {name: 'Pusher', vote: 0}, 
-      {name: 'Fanouts', vote: 0}, 
-      {name: 'Hydna', vote: 0}, 
-      {name: 'PubNub', vote: 0}, 
-      {name: 'Realtime.co', vote: 0}, 
-      {name: 'Reappt', vote: 0}, 
-      {name: 'Streamdata.io', vote: 0}, 
-      {name: 'Tambur.io', vote: 0}, 
-      {name: 'Convergence', vote: 0}, 
-      {name: 'Meteor', vote: 0}
-    ],
+      choise: 0,
+      added: [],
+      subscription: null,
+      like: 0,
+      extensions: [
+        {name: 'Ably', added: false, subscription: 0}, 
+        {name: 'Pusher', added: false, subscription: 1}, 
+        {name: 'Fanouts', added: false, subscription: 2}, 
+        {name: 'Hydna', added: false, subscription: 3}, 
+        {name: 'Pubnub', added: false, subscription: 4}, 
+        {name: 'Realtime.co', added: false, subscription: 5}, 
+        {name: 'Reappt', added: false, subscription: 6}, 
+        {name: 'Streamdata.io', added: false, subscription: 7}, 
+        {name: 'Tambur.io', added: false, subscription: 8}, 
+        {name: 'Convergence', added: false, subscription: 9}, 
+        {name: 'Meteor', added: false, subscription: 10}
+      ],
       description: 'Real-Time Technologies',
     },
     reducers: {
       choise: (state, Payload) => {
         if(Payload === 'Like') {
           return Object.assign({}, state, {
-            choise: 'UnLike',
-            // vote: state.vote
+            choise: 'UnLike'
           })
         } 
         if(Payload === 'UnLike') {
           return Object.assign({}, state, {
-            choise: 'Like',
-            // vote: state.vote
+            choise: 'Like'
           })
         }
         if(Payload === 'vote') {
@@ -38,56 +39,94 @@ export default {
         }
       },
       read: (state, Payload) => {
-        switch(Payload) {
-          case 'Ably': 
-            return Object.assign({}, state, {
-            description: 'Ably is a platform that makes it easy for you add realtime messaging and streaming data to your application. The global service is uniquely able to offer solutions to the most difficult aspects of messaging at scale such as limitless scale, connection state recovery, guaranteed message delivery, client library "intelligence" to work around transient network problems. The Ably service operates in more than 24 data centres globally offering the lowest latencies globally.' 
-            }) 
-          case 'Pusher':
-            return Object.assign({}, state, {
-            description: 'Pusher is a hosted API for quickly, easily and securely adding scalable realtime functionality to web and mobile apps.'
-            })
-          case 'Fanouts':
-            return Object.assign({}, state, {
-            description: 'For front-end applications, Fanouts JavaScript library receives realtime JSON notifications with just a few lines of code. Integration is quick and simple.'
-            })
-          case 'Hydna':
-            return Object.assign({}, state, {
-            description: 'Hydna is a hosted backend into which you can send data and have it instantly appear on other devices.Instantly move data across platforms, technologies, and devices. Hydna is ideal for building dashboards, activity streams, notification- and chat systems, real-time collaboration, live statistics, remote controls, multiplayer games, and more.'
-            })
-          case 'Pubnub':
-            return Object.assign({}, state, {
-            description: 'Pubnub is the fastest cloud-hosted realtime messaging system for web and mobile apps.'
-            })
-          case 'Realtime.co':
-            return Object.assign({}, state, {
-            description: 'The Realtime Messaging Framework is a cloud-hosted messaging system for websites and mobile apps that require constant content updates in just a few milliseconds, enabling any application to interact with millions of connected users in a fast and secure way.'
-            })
-          case 'Reappt':
-            return Object.assign({}, state, {
-            description: 'Reappt is the only enterprise-class Realtime Messaging service delivered via the cloud to power business-critical internet apps. Reappt helps you develop reactive applications faster with lower costs and reduced risks. It is designed for a broad set of internet, mobile, and IoT developers – and provides a complete set of tools, including APIs and client libraries.'
-            })
-          case 'Streamdata.io':
-            return Object.assign({}, state, {
-            description: 'Streamdata.io is a real-time cache proxy allowing you to poll JSON REST APIs and push updates to clients. But wait, there is more: Streamdata.io keeps a history of modifications that occur on the data between two pollings! This way, streamdata.io is able to give you the list of modifications which happened since last time you fetched the data.'
-            })
-          case 'Tambur.io':
-            return Object.assign({}, state, {
-            description: 'Tambur.io provides your business with a simple messaging API to build scalable realtime web and mobile apps.'
-            })
-          case 'Convergence':
-            return Object.assign({}, state, {
-            description: 'Convergence is the worlds first API specifically designed for realtime collaborative applications. It was created as a one-stop shop for realtime collaborative application development, with a rich JSON-inspired data model API, guaranteed conflict resolution, and first-class APIs for users, presence, chat, and collaboration awareness.'
-            })
-          case 'Meteor':
-            return Object.assign({}, state, {
-            description: 'Meteor is a set of new technologies for building top-quality web apps in a fraction of the time, whether you are an expert developer or just getting started.'
-            })      
-          default:
-            return Object.assign({}, state, {
-            description: 'Real-Time Technologies.'
-            })     
-        }
+        // console.log(Payload)
+          switch(Payload.name) {
+            case 'Ably': 
+              return Object.assign({}, state, {
+              description: 'Ably is a platform that makes it easy for you add realtime messaging and streaming data to your application. The global service is uniquely able to offer solutions to the most difficult aspects of messaging at scale such as limitless scale, connection state recovery, guaranteed message delivery, client library "intelligence" to work around transient network problems. The Ably service operates in more than 24 data centres globally offering the lowest latencies globally.',
+              choise: Payload.rowno,
+              subscription: state.extensions[Payload.rowno].subscription  
+              }) 
+            case 'Pusher':
+              return Object.assign({}, state, {
+              description: 'Pusher is a hosted API for quickly, easily and securely adding scalable realtime functionality to web and mobile apps.',
+              choise: Payload.rowno,
+              subscription: state.extensions[Payload.rowno].subscription  
+              })
+            case 'Fanouts':
+              return Object.assign({}, state, {
+              description: 'For front-end applications, Fanouts JavaScript library receives realtime JSON notifications with just a few lines of code. Integration is quick and simple.',
+              choise: Payload.rowno,
+              subscription: state.extensions[Payload.rowno].subscription  
+              })
+            case 'Hydna':
+              return Object.assign({}, state, {
+              description: 'Hydna is a hosted backend into which you can send data and have it instantly appear on other devices.Instantly move data across platforms, technologies, and devices. Hydna is ideal for building dashboards, activity streams, notification- and chat systems, real-time collaboration, live statistics, remote controls, multiplayer games, and more.',
+              choise: Payload.rowno,
+              subscription: state.extensions[Payload.rowno].subscription  
+              })
+            case 'Pubnub':
+              return Object.assign({}, state, {
+              description: 'Pubnub is the fastest cloud-hosted realtime messaging system for web and mobile apps.',
+              choise: Payload.rowno,
+              subscription: state.extensions[Payload.rowno].subscription  
+              })
+            case 'Realtime.co':
+              return Object.assign({}, state, {
+              description: 'The Realtime Messaging Framework is a cloud-hosted messaging system for websites and mobile apps that require constant content updates in just a few milliseconds, enabling any application to interact with millions of connected users in a fast and secure way.',
+              choise: Payload.rowno,
+              subscription: state.extensions[Payload.rowno].subscription  
+              })
+            case 'Reappt':
+              return Object.assign({}, state, {
+              description: 'Reappt is the only enterprise-class Realtime Messaging service delivered via the cloud to power business-critical internet apps. Reappt helps you develop reactive applications faster with lower costs and reduced risks. It is designed for a broad set of internet, mobile, and IoT developers – and provides a complete set of tools, including APIs and client libraries.',
+              choise: Payload.rowno,
+              subscription: state.extensions[Payload.rowno].subscription  
+              })
+            case 'Streamdata.io':
+              return Object.assign({}, state, {
+              description: 'Streamdata.io is a real-time cache proxy allowing you to poll JSON REST APIs and push updates to clients. But wait, there is more: Streamdata.io keeps a history of modifications that occur on the data between two pollings! This way, streamdata.io is able to give you the list of modifications which happened since last time you fetched the data.',
+              choise: Payload.rowno,
+              subscription: state.extensions[Payload.rowno].subscription  
+              })
+            case 'Tambur.io':
+              return Object.assign({}, state, {
+              description: 'Tambur.io provides your business with a simple messaging API to build scalable realtime web and mobile apps.',
+              choise: Payload.rowno,
+              subscription: state.extensions[Payload.rowno].subscription  
+              })
+            case 'Convergence':
+              return Object.assign({}, state, {
+              description: 'Convergence is the worlds first API specifically designed for realtime collaborative applications. It was created as a one-stop shop for realtime collaborative application development, with a rich JSON-inspired data model API, guaranteed conflict resolution, and first-class APIs for users, presence, chat, and collaboration awareness.',
+              choise: Payload.rowno,
+              subscription: state.extensions[Payload.rowno].subscription  
+              })
+            case 'Meteor':
+              return Object.assign({}, state, {
+              description: 'Meteor is a set of new technologies for building top-quality web apps in a fraction of the time, whether you are an expert developer or just getting started.',
+              choise: Payload.rowno,
+              subscription: state.extensions[Payload.rowno].subscription  
+              })
+            case 'subscribe':
+              return {
+                ...state,
+                [state.extensions[state.choise]]: {
+                    subscription: state.extensions[state.choise].subscription + 1
+                }
+              }
+              // console.log(state.subscription)  
+              // return Object.assign({}, state, {
+              // subscription: state.subscription + 1
+              // })
+              // let newState = Object.assign({}, state);
+              // newState.extensions[state.choise].subscription = newState.extensions[state.choise].subscription + 1 
+              // return Object.assign({},state, newState)
+            default:
+              return Object.assign({}, state, {
+              description: 'Real-Time Technologies.',
+              choise: 'default'
+              })     
+          }
       }
     }
   }  
